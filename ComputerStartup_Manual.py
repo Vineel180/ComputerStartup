5249b548e9ae587248de2ad6da0f1360a3728a42
Vineel180 <vineel180@gmail.com>
2024-12-31 04:49:10 +0530

    v1b1p2

---
+++ ComputerStartup_Manual.py
@@ -0,0 +1,8 @@
+userInput = list(input("0: Daily. 1: Weekly. 2: Monthly. 3: Quarterly. 4: Yearly. | "))
+
+if "0" in userInput:
+if "1" in userInput:
+if "2" in userInput:
+if "3" in userInput:
+if "4" in userInput:
+if "0" in userInput:
